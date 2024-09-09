baf2a48b9a0309d2b247238963c3deccafe621f1
Mike Izbicki <mike@izbicki.me>
2024-09-09 09:05:33 -0700

    update solution

--- ragnews.py
+++ ragnews.py
@@ -1,7 +1,10 @@
 #!/bin/python3
 
 '''
-Run an interactive QA session with the news articles contained in the specified database.
+Run an interactive QA session with the news articles using the Groq LLM API and retrieval augmented generation (RAG).
+
+New articles can be added to the database with the --add_url parameter,
+and the path to the database can be changed with the --db parameter.
 '''
 
 from bs4 import BeautifulSoup
@@ -49,9 +52,9 @@ def run_llm(system, user, model='llama3-8b-8192', seed=None):
     return chat_completion.choices[0].message.content
 
 
-def summarize_text(text):
+def summarize_text(text, seed=None):
     system = 'Summarize the input text below.  Limit the summary to 1 paragraph.  Use an advanced reading level similar to the input text, and ensure that all people, places, and other proper and dates nouns are included in the summary.  The summary should be in English.'
-    return run_llm(system, text)
+    return run_llm(system, text, seed=seed)
 
 
 def translate_text(text):
@@ -69,7 +72,18 @@ def extract_keywords(text, seed=None):
     'Joe candidate nominee presidential Democrat election primary TBD voting politics'
     >>> extract_keywords('What is the policy position of Trump related to illegal Mexican immigrants?', seed=0)
     'Trump Mexican immigrants policy position illegal border control deportation walls'
+
+    Note that the examples above are passing in a seed value for deterministic results.
+    In production, you probably do not want to specify the seed.
     '''
+
+    # FIXME:
+    # Implement this function.
+    # It's okay if you don't get the exact same keywords as me.
+    # You probably certainly won't because you probably won't come up with the exact same prompt as me.
+    # To make the test cases above pass,
+    # you'll have to modify them to be what the output of your prompt provides.
+
     system = 'Extract the most important 10 words from the user prompt.  All names and other proper nouns should be included in the output list.  For non-proper nouns, synonyms and related words should also appear in the output.  Each word should appear on a line by itself, with no numbering or other formatting.  Do not provide an explanation of what you did, only provide the list of words in the proper format.'
     user = text
     rex = re.compile(r'\W+')
@@ -88,6 +102,11 @@ def _logsql(sql):
 
 
 def _catch_errors(func):
+    '''
+    This function is intended to be used as a decorator.
+    It traps whatever errors the input function raises and logs the errors.
+    We use this decorator on the add_urls method below to ensure that a webcrawl continues even if there are errors.
+    '''
     def inner_function(*args, **kwargs):
         try:
             func(*args, **kwargs)
@@ -107,6 +126,21 @@ def rag(text, db):
     The db argument should be an instance of the `ArticleDB` class that contains the relevant documents to use.
     '''
 
+    # FIXME:
+    # Implement this function.
+    # Recall that your RAG system should:
+    # 1. Extract keywords from the text.
+    # 2. Use those keywords to find articles related to the text.
+    # 3. Construct a new user prompt that includes all of the articles and the original text.
+    # 4. Pass the new prompt to the LLM and return the result.
+    #
+    # HINT:
+    # You will also have to write your own system prompt to use with the LLM.
+    # I needed a fairly long system prompt (about 15 lines) in order to get good results.
+    # You can start with a basic system prompt right away just to check if things are working,
+    # but don't spend a lot of time on the system prompt until you're sure everything else is working.
+    # Then, you can iteratively add more commands into the system prompt to correct "bad" behavior you see in your program's output.
+
     keywords = extract_keywords(text)
 
     articles = db.find_articles(keywords)
@@ -124,8 +158,8 @@ QUESTION: {text}
     
     system = f'You are a professional news commentator answering questions about current events.  Your audience is well educated American citizens, but they have not been following the news closely.  Below, you are given a question and several related news articles.   The current date is now {datetime.datetime.now().isoformat()}, and these news articles are all contain new information about the world since you were last trained.  Your job is to answer the question in an unbiased manner using the new information provided in the news articles.  As much as practical, you should use only knowledge contained in the news articles and not background knowledge, because the background knowledge might be out of date.  Your final answer should not mention that you are summarizing news articles, just provide the information the way a professional news commentator would provide it.  Do not provide citations.  Your answers should be short and to the point.  They should be at most 1 paragraph, and preferably only 1-2 sentences.  Do not provide any extra commentary unless asked.  It is okay to provide opinions about controversial subjects such as who to vote for as long as those opinions are grounded in facts specified in both the user prompt and the news articles.  If there is not enough information in the user prompt, then ask for the specified information to be provided.'
 
-    logging.debug('rag.SYSTEM: ' + system)
-    logging.debug('rag.USER: ' + user)
+    logging.info('rag.SYSTEM: ' + system)
+    logging.info('rag.USER: ' + user)
 
     return run_llm(system, user)
 
@@ -208,6 +242,19 @@ class ArticleDB:
 
         Lowering the value of the timebias_alpha parameter will result in the time becoming more influential.
         '''
+        
+        # FIXME:
+        # Implement this function.
+        # You do not need to concern yourself with the timebias_alpha parameter.
+        #
+        # HINT:
+        # The only thing my solution does is pass a SELECT statement to the sqlite3 database.
+        # The SELECT statement will need to use sqlite3's FTS5 syntax for full text search.
+        # If you need to review how to coordinate sqlite3 and python,
+        # there is an example in the __len__ method below.
+        # The details of the SELECT statement will be different
+        # (because the functions collect different information)
+        # but the outline of the python code is the same.
 
         sql = '''
         SELECT
